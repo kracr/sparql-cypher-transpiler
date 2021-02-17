c858cf4c8bcc1860e9fba543cc4a26fff0b3c31a
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2021-02-18 03:17:28 +0530

    Add Maven

---
+++ src/test/java/com/KRacR/s2c/AppTest.java
@@ -0,0 +1,20 @@
+package com.KRacR.s2c;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
