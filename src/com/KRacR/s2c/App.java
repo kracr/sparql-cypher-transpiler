aaafbaef4b9ac532842c42eb245004100715a830
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2021-03-17 20:07:05 +0530

    Add sample query

--- src/com/KRacR/s2c/App.java
+++ src/com/KRacR/s2c/App.java
@@ -1,8 +1,13 @@
 package com.KRacR.s2c;
 
 import org.neo4j.cypher.*;
+import org.neo4j.cypher.internal.ast.Query;
+import org.apache.jena.*;
 
 public class App {
     public static void main( String[] args ){
+    	Query q = new Query(null, null, null);
+    	System.out.println(q.asCanonicalStringVal());
+    	org.apache.jena.query.Query q2 = new org.apache.jena.query.Query();
     }
 }
