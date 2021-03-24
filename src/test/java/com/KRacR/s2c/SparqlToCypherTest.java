45956e7b9c443b4345e09837967b0de55a6d3bee
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2021-03-25 00:06:26 +0530

    Added neo4j in memory database

--- src/test/java/com/KRacR/s2c/SparqlToCypherTest.java
+++ src/test/java/com/KRacR/s2c/SparqlToCypherTest.java
@@ -1,8 +1,10 @@
 package test.java.com.KRacR.s2c;
 
+//import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.neo4j.graphdb.Label.label;
 
 import java.io.File;
 import java.io.FileFilter;
@@ -14,11 +16,25 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Value;
+import org.neo4j.driver.v1.Values;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.harness.ServerControls;
+import org.neo4j.harness.TestServerBuilders;
 
 /**
  * Unit test for Sparql to Cypher
  */
 public class SparqlToCypherTest{
+	private ServerControls embeddedDatabaseServer;
+	private static final Config driverConfig = Config.build().withoutEncryption().toConfig();
+	
 	private void run_TTL_Automated_Test(String folder) {
 		Path rdf_path = Paths.get(folder, "rdf.ttl");
 		Path path_to_pg_bench = Paths.get("lib/RDFtoPGConverter.jar");
@@ -55,8 +71,21 @@ public class SparqlToCypherTest{
 		};
 
 		File[] files = f.listFiles(directoryFilter);
+		setupNeo4jServer();
 		for (File folder : files) {
+			delete_all_from_database();
 			run_TTL_Automated_Test(folder.getCanonicalPath());
 		}
     }
+
+	private void delete_all_from_database() {
+		try (Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
+				Session session = driver.session()) {
+			session.run("match (n) detach delete n;");
+		}
+	}
+
+	private void setupNeo4jServer() {
+		this.embeddedDatabaseServer = TestServerBuilders.newInProcessBuilder().withProcedure(apoc.load.LoadJson.class).newServer();
+	}
 }
