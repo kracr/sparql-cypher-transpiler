e62099e68cc75f6e875df944fc97ea5dee4aa018
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2021-03-24 11:35:21 +0530

    Start work on testing framework

--- src/test/java/com/KRacR/s2c/AppTest.java
+++ src/test/java/com/KRacR/s2c/AppTest.java
@@ -1,6 +1,12 @@
-package com.KRacR.s2c;
+package test.java.com.KRacR.s2c;
 
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
@@ -9,12 +15,26 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+	private void run_TTL_Automated_Test(String folder) {
+		Path rdf_path = Paths.get(folder, "rdf.ttl");
+		System.out.println(rdf_path);
+	}
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void All_TTL_Automated_Test() throws IOException{
+    	// Code for iterating over all directories:
+    	// Source: http://www.avajava.com/tutorials/lessons/how-do-i-use-a-filefilter-to-display-only-the-directories-within-a-directory.html
+    	File f = new File("src/test/resources/ttl_automated_tests"); // current directory
+
+		FileFilter directoryFilter = new FileFilter() {
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		};
+
+		File[] files = f.listFiles(directoryFilter);
+		for (File folder : files) {
+			run_TTL_Automated_Test(folder.getCanonicalPath());
+		}
     }
 }
